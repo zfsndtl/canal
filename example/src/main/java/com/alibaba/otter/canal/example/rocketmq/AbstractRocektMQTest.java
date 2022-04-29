a47a63c51ecaf21f8a9dc3a4c4b3a2f0ea6f812c
dufangzhou <dufangzhou@hzanchu.com>
2022-04-29 15:36:31 +0800

    canal +rocket测试

--- example/src/main/java/com/alibaba/otter/canal/example/rocketmq/AbstractRocektMQTest.java
+++ example/src/main/java/com/alibaba/otter/canal/example/rocketmq/AbstractRocektMQTest.java
@@ -5,11 +5,19 @@ import com.alibaba.otter.canal.example.BaseCanalClientTest;
 public abstract class AbstractRocektMQTest extends BaseCanalClientTest {
 
     public static String  topic              = "example";
-    public static String  groupId            = "group";
-    public static String  nameServers        = "127.0.0.1:9876";
+    public static String  groupId            = "canal";
+    public static String  nameServers        = "192.168.120.110:9876";
     public static String  accessKey          = "";
     public static String  secretKey          = "";
     public static boolean enableMessageTrace = false;
-    public static String  accessChannel      = "local";
+    public static String  accessChannel      = "local";//
     public static String  namespace          = "";
 }
+//rocketmq.producer.group = canal
+//rocketmq.enable.message.trace = false
+//rocketmq.customized.trace.topic = example
+//rocketmq.namespace =
+//rocketmq.namesrv.addr = 192.168.120.110:9876
+//rocketmq.retry.times.when.send.failed = 0
+//rocketmq.vip.channel.enabled = false
+//rocketmq.tag =
